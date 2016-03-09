e952878938dd369746e79bd4c666b35b5185d3ac
akhmelov <akhmelow@gmail.com>
2016-03-09 12:34:24 +0100

    created project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Koda_Huffman_Simple
 Simple Huffman's algorithm with coder and decoder
+
+/bin
+/include
+/obj
+
+*.cbp
+*.depend
+*.layout
+
+
---
+++ include/Coder.h
@@ -0,0 +1,15 @@
+#ifndef CODER_H
+#define CODER_H
+
+class Coder
+{
+    public:
+
+        Coder();
+        virtual ~Coder();
+
+    protected:
+    private:
+};
+
+#endif // CODER_H
