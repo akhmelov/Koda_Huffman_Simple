58100307822289c1066842f21c161f7d79502164
Wiktor Kuśmirek <wiktor93@onet.com.pl>
2016-03-14 18:23:57 +0100

    simple (pseudo)random sequence generator added

--- include/ParametersService.h
+++ include/ParametersService.h
@@ -14,12 +14,18 @@ public:
 
     unsigned int getOperationType();
 
+    unsigned int getSequenceLength();
+
+    unsigned int getNumOfCharsInAlphabet();
+
     void service(int argc, const char* argv[]);
 
 private:
     std::string inputFileName_;
     std::string outputFileName_;
     unsigned int operationType_;
+    unsigned int numOfCharsInAlphabet_;
+    unsigned int sequenceLength_;
 
     void printHelp();
 };
